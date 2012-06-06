ac5e1ce7f9f3d83a946fa03008e73c561f55d2eb
Zach Kost-Smith <zachkostsmith@gmail.com>
2012-06-06 05:43:38 -0500

    Added some string manipulation utilities.
    Functions ./ , ~/ , and /./ allow you to write paths easily.  We'll see if they
    are useful.

--- cmd.lisp
+++ cmd.lisp
@@ -53,12 +53,46 @@
 ;;   (with-open-stream 
 ;;       (%cmd command :output 
 
+;; Some string utilities
+
+(defun ~/ (&rest args)
+  (concatenate
+   'string
+   "$HOME/" (apply #'./ (rest args))))
+
+(defun /./ (&rest args)
+  (concatenate
+   'string
+   "/" (apply #'./ (rest args))))
+
+(defgeneric stringify (obj)
+  (:method (obj)
+    (with-output-to-string (out)
+      (princ obj out))))
+
+(defun ./ (&rest args)
+  (if (null (rest args))
+      (first args)
+      (concatenate
+       'string
+       (first args)
+       "/"
+       (apply #'./ (rest args)))))
+
+(defvar _ " ")
+
 (defun %mkdstr (delim &rest args)
   "Make delimited string"
   (with-output-to-string (out)
     (iter (for a in args)
       (unless (first-iteration-p)
         (princ (stringify delim) out))
+      (princ a out))))
+
+(defun %mkstr (&rest args)
+  "Make string"
+  (with-output-to-string (out)
+    (iter (for a in args)
       (princ a out))))
 (defvar *trim-whitespace* t)
 
